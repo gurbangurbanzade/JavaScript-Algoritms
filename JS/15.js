6107d5763b8e8b3e8e90381c91ea322f44a2f2a0
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-30 01:45:17 +0400

    Arrayin en boyuk elementinin tapilmasi

---
+++ JS/15.js
@@ -0,0 +1,23 @@
+// arreyin en boyuk elementinin tapilmasi
+let arr = [-12, -34, 0, 9, -56, -1, 1, 9, 6, 3];
+let maxi;
+let maxj;
+let max;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] > arr[j]) {
+      console.log(`maximum ${arr[i]}`);
+      maxi = arr[i];
+      break;
+    } else {
+      console.log(`maximum ${arr[j]}`);
+      maxj = arr[j];
+      break;
+    }
+  }
+}
+if (maxi > maxj) {
+  alert(`Ən böyük ədəd - ${maxi}`);
+} else {
+  alert(`Ən böyük ədəd - ${maxj}`);
+}
