9b76832c1402192376cf5bc45152d0d61ceebda7
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-29 23:42:24 +0400

    Pensiyaya neçə il qalıb hesablama

--- JS/12.js
+++ JS/12.js
@@ -1,2 +1,18 @@
-let a = Math.floor(Math.random() * 100);
-console.log(a);
+function yasHesabla(dogumili) {
+  return 2022 - dogumili;
+}
+
+let yas = yasHesabla(1993);
+//console.log(yas);
+
+function pensiyaHesabla(dogumili, ad) {
+  yas = yasHesabla(dogumili);
+  let pen = 65 - yas;
+  if (pen > 0) {
+    console.log(`${ad} pensiyaya cixmaga ${pen} il qalib`);
+  } else {
+    console.log(`${ad} artiq pensiyaya cixmisiniz`);
+  }
+}
+
+pensiyaHesabla(1997, "Gülşən");
